979f327149f627bc4be5a43cb8fbe33956ad888f
Daniel Nesbitt <daniel.nesbitt@genedata.com>
2020-01-15 17:41:20 -0500

    Sync user state from the server on page start.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./App.css";
 import {Route, Router, Switch} from "react-router";
-import {createBrowserHistory} from 'history';
+import {createBrowserHistory} from "history";
 import {Home} from "./routes/Home/Home";
 import {Question} from "./routes/Question";
-import {Provider} from "react-redux";
+import {Provider, useDispatch} from "react-redux";
 import {Login} from "./routes/Login/Login";
-import {userSelector} from "./routes/Login/LoginModule";
+import {loginSuccessfulAction, userSelector} from "./routes/Login/LoginModule";
 import {store, useTypedSelector} from "./state/Store";
+import axios, {AxiosResponse} from "axios";
+import {Spinner, SpinnerSize, Stack} from "office-ui-fabric-react";
 
 const history = createBrowserHistory();
 
-const Routing: React.FC = () => <Router history={history}>
+const Routing: React.FC = () => (<Router history={history}>
     <Switch>
         <Route path="/question/:id">
             <Question/>
@@ -20,17 +22,36 @@ const Routing: React.FC = () => <Router history={history}>
             <Home/>
         </Route>
     </Switch>
-</Router>;
+</Router>);
+
+const AppSpinner: React.FC = () => (
+    <Stack style={{height: "100%"}} verticalAlign="center">
+        <Spinner size={SpinnerSize.large}/>
+    </Stack>
+);
 
 const AppView: React.FC = () => {
+    const [loaded, setLoaded] = useState(false);
+    const dispatch = useDispatch();
+    const dispatchLogin = (response: AxiosResponse) => {
+        dispatch(loginSuccessfulAction(response.data, response.data))
+    };
+
+    useEffect(() => {
+        axios("/api/user").then(response => {
+            if (!!response.data) {
+                dispatchLogin(response)
+            }
+            setLoaded(true);
+        });
+    });
+
     const user = useTypedSelector(userSelector);
+    const Body = !!user ? <Routing/> : <Login/>;
     return (
         <div className="App">
-            {!!user
-                ? <Routing/>
-                : <Login/>
-            }
             {user ? <h1>{user}</h1> : null}
+            {loaded ? Body : <AppSpinner/>}
         </div>
     );
 };
