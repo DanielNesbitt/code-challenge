71048e2a15d9903dc79e36020fe0489a54b0c07d
Daniel Nesbitt <daniel.nesbitt@genedata.com>
2020-01-15 17:41:36 -0500

    Display questions in a table.

--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {DetailsList, IColumn} from "office-ui-fabric-react";
+import {DetailsList, IColumn, SelectionMode, Stack} from "office-ui-fabric-react";
 import {questionsSelector} from "./HomeModule";
 import {QuestionEntry} from "../../state/ServerRPC";
 import {useTypedSelector} from "../../state/Store";
@@ -8,22 +8,26 @@ const columns: IColumn[] = [
     {
         key: 'question_state',
         name: 'Answered',
+        maxWidth: 100,
     } as IColumn,
     {
         key: 'question',
         name: 'Question',
-        data: 'questionTitle',
+        fieldName: 'questionTitle',
     } as IColumn
 ];
 
 export const Home: React.FC = () => {
     const questions: QuestionEntry[] | undefined = useTypedSelector(questionsSelector);
-    return <div>
-        {questions ?
-            <DetailsList
-                items={questions}
-                columns={columns}
-            />
-            : null}
-    </div>;
+    return <Stack horizontal horizontalAlign="center" className="ms-depth-8" style={{margin: "20px"}}>
+        <div style={{width: "100%", padding: "10px"}}>
+            {questions ?
+                <DetailsList
+                    selectionMode={SelectionMode.none}
+                    items={questions}
+                    columns={columns}
+                />
+                : null}
+        </div>
+    </Stack>;
 };
