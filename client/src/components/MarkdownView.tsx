6deed589150db6ce1e585d1ac49bb6504478403e
Daniel Nesbitt <daniel.nesbitt@genedata.com>
2020-02-06 09:53:38 -0500

    Use github style markdown for tables.

--- client/src/components/MarkdownView.tsx
+++ client/src/components/MarkdownView.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import {tomorrow} from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import 'github-markdown-css'
 
 type CodeHighlighterProps = {
     value: string,
@@ -17,5 +18,6 @@ const CodeHighlighter: React.FC<CodeHighlighterProps> = ({value, language}) => {
 export type MarkdownViewProps = { markdown: string }
 
 export const MarkdownView: React.FC<MarkdownViewProps> = ({markdown}) => (
-    <ReactMarkdown source={markdown} renderers={{code: CodeHighlighter}}/>
+    <ReactMarkdown source={markdown}
+                   className="markdown-body" renderers={{code: CodeHighlighter}}/>
 );
