16cf335faed8eef900279f8f524ea0d36c7002b7
Daniel Nesbitt <dan.nesbitt@gmail.com>
2021-02-03 17:18:26 -0500

    Revert "Update docker file to build the application." Revert "Try to update gradle frontend plugin." Revert "Update js deps."
    This reverts commit 50d93342
    This reverts commit 9e3ad57b
    This reverts commit 2fef6b4b

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,67 +20,66 @@ const history = createBrowserHistory();
 
 const Routing: React.FC = () => (
     <Switch>
-      <Route path="/secretAdminRoute">
-        <Admin/>
-      </Route>
-      <Route path="/question/:id">
-        <QuestionView/>
-      </Route>
-      <Route path="/">
-        <Home/>
-      </Route>
+        <Route path="/secretAdminRoute">
+            <Admin/>
+        </Route>
+        <Route path="/question/:id">
+            <QuestionView/>
+        </Route>
+        <Route path="/">
+            <Home/>
+        </Route>
     </Switch>
 );
 
 const createBreadCrumb = (location: Location, question: Question | undefined): IBreadcrumbItem[] => {
-  const crumb: IBreadcrumbItem[] = [
-    {
-      text: 'Home', key: 'Home', onClick: () => {
-        history.push('/')
-      }
-    },
-  ];
-  const paths = location.pathname.trim().split('/');
-  if (paths[1] === 'question' && !!question) {
-    crumb.push({text: question?.title, key: question?.title});
-  }
-  return crumb;
+    const crumb: IBreadcrumbItem[] = [
+        {
+            text: 'Home', key: 'Home', onClick: () => {
+                history.push('/')
+            }
+        },
+    ];
+    const paths = location.pathname.trim().split('/');
+    if (paths[1] === 'question' && !!question) {
+        crumb.push({text: question?.title, key: question?.title});
+    }
+    return crumb;
 };
 
 const AppView: React.FC = () => {
-  const [loaded, setLoaded] = useState(false);
-  const dispatch = useDispatch();
+    const [loaded, setLoaded] = useState(false);
+    const dispatch = useDispatch();
 
-  const location = useLocation();
-  const question = useTypedSelector(questionSelector);
-  const items = createBreadCrumb(location, question);
+    const location = useLocation();
+    const question = useTypedSelector(questionSelector);
+    const items = createBreadCrumb(location, question);
 
-  const connected = useTypedSelector(socketStatusSelector) === SocketStatus.Connected;
-  const user = useTypedSelector(userSelector);
+    const connected = useTypedSelector(socketStatusSelector) === SocketStatus.Connected;
+    const user = useTypedSelector(userSelector);
 
-  useEffect(() => {
-    axios("/api/user").then(response => {
-      if (!!response.data) {
-        dispatch(loginSuccessfulAction(response.data, response.data))
-      }
-      setLoaded(true);
-    });
-  }, [dispatch]);
+    useEffect(() => {
+        axios("/api/user").then(response => {
+            if (!!response.data) {
+                dispatch(loginSuccessfulAction(response.data, response.data))
+            }
+            setLoaded(true);
+        });
+    }, [dispatch]);
 
-  const Body = !!user ? <Routing/> : <Login/>;
-  return (
-      <div className="App" style={{padding: "20px"}}>
-        {user ? <Breadcrumb items={items} style={{padding: "0px 0px 10px 0px"}}/> : null}
-        {!loaded || (!!user && !connected) ? <PageSpinner/> : Body}
-      </div>
-  );
+    const Body = !!user ? <Routing/> : <Login/>;
+    return (
+        <div className="App" style={{padding: "20px"}}>
+            {user ? <Breadcrumb items={items} style={{padding: "0px 0px 10px 0px"}}/> : null}
+            {!loaded || (!!user && !connected) ? <PageSpinner/> : Body}
+        </div>
+    );
 };
 
 export const App: React.FC = () => (
     <Provider store={store}>
-      <Router history={history}>
-        <AppView/>
-      </Router>
+        <Router history={history}>
+            <AppView/>
+        </Router>
     </Provider>
 );
-
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import "office-ui-fabric-core/dist/css/fabric.css"
 import {initializeIcons} from 'office-ui-fabric-react/lib/Icons';
 import './index.css';
-import { App } from './App';
+import {App} from './App';
 
 initializeIcons();
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
